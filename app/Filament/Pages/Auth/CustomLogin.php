3bd89fb0cccfc650fbb323b69976b1aa9b1daff7
iurygdeoliveira <iurygdeoliveira@gmail.com>
2025-05-01 00:55:43 +0000

    adicionando validação de tempo real no login

--- app/Filament/Pages/Auth/CustomLogin.php
+++ app/Filament/Pages/Auth/CustomLogin.php
@@ -5,19 +5,22 @@ declare(strict_types = 1);
 namespace App\Filament\Pages\Auth;
 
 use Filament\Forms\Components\Component;
-use Filament\Forms\Components\TextInput;
 use Filament\Pages\Auth\Login as BaseLogin;
 
 class CustomLogin extends BaseLogin
 {
+    // Adicionando validação em tempo real
     protected function getEmailFormComponent(): Component
     {
-        return TextInput::make('email')
-            ->label(__('filament-panels::pages/auth/login.form.email.label'))
-            ->email()
-            ->required()
-            ->autocomplete()
-            ->autofocus()
-            ->extraInputAttributes(['tabindex' => 1]);
+        $component = parent::getEmailFormComponent();
+        $component
+            ->live(onBlur: true)
+            ->afterStateUpdated(
+                function (CustomLogin $livewire) {
+                    $livewire->validateOnly('data.email');
+                }
+            );
+
+        return $component;
     }
 }
