5d7339f4b2f508764e0e55571808fa0816954b74
iurygdeoliveira <iurygdeoliveira@gmail.com>
2025-05-13 20:29:10 +0000

    realizando otimizações

--- app/Filament/Resources/UserResource/RelationManagers/RolesRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/RolesRelationManager.php
@@ -4,11 +4,14 @@ declare(strict_types = 1);
 
 namespace App\Filament\Resources\UserResource\RelationManagers;
 
+use App\Models\Permission;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 
 class RolesRelationManager extends RelationManager
 {
@@ -27,26 +30,49 @@ class RolesRelationManager extends RelationManager
 
     public function table(Table $table): Table
     {
+
+        $permissions = Permission::all();
+        
+        $columns = [
+            TextColumn::make('name')
+                ->label('Funções')
+                ->sortable(),
+        ];
+        
+        // Adiciona colunas dinâmicas de permissões com toggles
+        foreach ($permissions as $permission) {
+            $columns[] = Tables\Columns\ToggleColumn::make('permissions.' . $permission->id)
+                ->label($permission->name)
+                ->afterStateUpdated(function (RelationManager $livewire, Model $record, $state) use ($permission) {
+                    if ($state) {
+                        $record->permissions()->attach($permission->id);
+                    } else {
+                        $record->permissions()->detach($permission->id);
+                    }
+                })
+                ->getStateUsing(function (Model $record) use ($permission): bool {
+                    return $record->permissions->contains('id', $permission->id);
+                })
+                ->alignCenter();
+        }
+        
         return $table
             ->recordTitleAttribute('name')
-            ->label('Permissões em cada função')
-            ->columns([
-                Tables\Columns\TextColumn::make('name'),
-            ])
+            ->defaultPaginationPageOption(11)
+            ->paginated([11, 'all'])
+            ->columns($columns)
             ->filters([
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+               
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+               
+            ])
+            ->defaultSort('name', 'asc');
     }
 }
